8701bea4203d5adb35d09e06a2593f1136791076
Berk <berktopcu@outlook.com>
2021-07-09 14:40:07 +0300

    initial commit

---
+++ src/main/java/com/yataygecisle/user/transactions/YgUserTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.yataygecisle.user.transactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YgUserTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YgUserTransactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yataygecisle/user/transactions/YgUserTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yataygecisle.user.transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YgUserTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
